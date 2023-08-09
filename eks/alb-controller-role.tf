a584e39700fcc5bd619af3427c29a01b049e5707
ecvheo1 <ecvheo@naver.com>
2023-08-09 13:37:16 +0900

    :memo: ALB IAM Role 생성 관련 주석 추가

--- eks/alb-controller-role.tf
+++ eks/alb-controller-role.tf
@@ -1,3 +1,4 @@
+# IAM Assumable Role 생성
 module "iam_assumable_role_alb_controller" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "5.0.0"
@@ -8,11 +9,12 @@ module "iam_assumable_role_alb_controller" {
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
 }
 
+# ALB Controller에 필요한 IAM 정책을 가져옴
 data "http" "iam_policy" {
   url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.7/docs/install/iam_policy.json"
 }
 
-# 인라인으로 정책이 추가
+# IAM Role에 IAM Policy 연결
 resource "aws_iam_role_policy" "controller" {
   name_prefix = "AWSLoadBalancerControllerIAMPolicy"
   policy      = data.http.iam_policy.body
